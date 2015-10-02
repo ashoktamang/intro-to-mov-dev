673b42ff46ced8b216ed2654e34963386e718e95
ashoktamang <ashoktamang52@gmail.com>
2015-10-02 16:53:20 -0400

    More parse REST API

---
+++ assignment2/credentials.js
@@ -0,0 +1,4 @@
+PARSE__HEADER_CREDENTIALS = {
+    "x-parse-application-id": "gz2Z1PWrwbHXbo7RmLImvr2KPS4hodNY7Xi69Vwu",
+    "x-parse-rest-api-key": "cn1vCkQZX9MNxeQyMzmZtSwr72kqevYQefAnjpL3"
+};
--- assignment2/services.js
+++ assignment2/services.js
@@ -1,65 +1,93 @@
-var myApp = angular.module('starter.services', ['ionic']);
+angular.module('starter.services', [])
 
-myApp.service("Parse", function ($http){ 
-  //Auth for parse account and details in it
-	var authenticationHeaders = {
-        "x-parse-application-id": "gz2Z1PWrwbHXbo7RmLImvr2KPS4hodNY7Xi69Vwu",
-        "x-parse-rest-api-key": "cn1vCkQZX9MNxeQyMzmZtSwr72kqevYQefAnjpL3"
-    };
+    .service("ParseService", function ($http) {
 
-  	var defaultSettings = {
-        "async": true,
-        "crossDomain": true,
-        headers: authenticationHeaders,
-    }; 
+        var baseURL = "https://api.parse.com/1/";
+        //Auth for parse account and details in it
+        var authenticationHeaders = PARSE__HEADER_CREDENTIALS;
 
-    var login = function() {
-    	// copies the object...
-      var settings = {
-        "async": true,
-        //"crossDomain": true,
-        "headers": authenticationHeaders,
-        "url": "https://api.parse.com/1/login",
-        "method": "GET",
-        "data": {
-          "username": "ashok.tamang@bison.howard.edu",
-          "password": "yamahaFX3000"
+        var defaultSettings = {
+            "async": true,
+            "crossDomain": true,
+            headers: authenticationHeaders,
+        };
+
+        var login = function () {
+            // copies the object...
+            var settings = {
+                "async": true,
+                //"crossDomain": true,
+                "headers": authenticationHeaders,
+                // "url": "https://api.parse.com/1/login",
+                // "method": "GET",
+                "data": {
+                    "username": "ashok.tamang@bison.howard.edu",
+                    "password": "yamahaFX3000"
+                }
+            };
+
+            $http.get(baseURL + "login", settings)
+                .then(function (response) {
+                    // In the response resp.data contains the result
+                    // check the console to see all of the data returned
+                    console.log('login', response);
+                    return response.data // for debug;
+                });
+        };
+
+
+        return {
+            getStuffList: function () {
+                // Returns a list of stuff objects
+
+                //Initializing an object settings to
+                var settings = angular.extend({}, defaultSettings);
+
+                // gets the data from Parse, if successful.
+                return $http.get(baseURL + "classes/stuff/", settings)
+                    .then(function (response) {
+                        console.log(response) //for debug;
+                        console.log(response.data); //for debug
+                        return response.data;
+                    });
+            },
+
+            getStuffId: function(_id) {
+                // Returns a stuff object by ID
+
+                var settings = angular.extend({}, defaultSettings);
+
+                // get the stuff object by id, if successful.
+                return $http.get(baseURL + "classes/stuff/" + _id, settings)
+                    .then(function (response) {
+                        console.log(response) //for debug;
+                        console.log(response.data) //for debug;
+                        return response.data;
+                    });
+            },
+
+            addStuff: function (stuff) {
+
+                // for POST, we only need to set the authentication header
+                var settings = {
+                    headers: authenticationHeaders,
+                };
+
+                var dataObject = {
+                    "name": stuff.name,
+                    "room": stuff.room,
+                };
+
+                var dataObjectString = JSON.stringify(dataObject);
+
+                // $http returns a promise, which has a then function
+                return $http.post(baseURL + 'classes/stuff', dataObjectString, settings)
+                    .then(function (response) {
+                        // In the response resp.data contains the result
+                        // check the console to see all of the data returned
+                        console.log('addObject', response);
+                        return response.data;
+                    });
+            },
         }
-      };
-      $http(settings)
-      // success callback function implemented as an
-      // anonymous function
-      .success(function(_successResponse) {
-        //alert(JSON.stringify(_successResponse));
-      })
-      // error callback function referenced as a defined
-      // function above
-      .error(function(_errorResponse) {
-        alert(JSON.stringify(res_errorResponseponse));
-      })
-      // done handler is called irregardless of an error or success
-      // this is a catch all
-      .done(function(response) {
-        console.log("DONE - " + JSON.stringify(response));
-      });
-    };
-
-  
-  return {
-    getStuffList: function(){
-		//Initiliazing an object settings to 
-		var settings = angular.extend({}, defaultSettings);
-
-		// sets the specific URL for $http.get method
-		settings.url = "https://api.parse.com/1/classes/stuff";
-
-		// sets the appropriate REST VERB for tha API call
-		settings.method = "GET";
-
-		// gets the data from Parse, if successfull
-		return $http(settings).success(function(response){
-		});
-	},
-
-  }
-});
+    })
